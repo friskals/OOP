49e18093b9006d476f773b36355ba685d42fb045
Friska Sianturi <friskasianturi23@gmail.com>
2020-06-25 15:17:25 +0700

    Create an object

---
+++ 1. class/AchievementBadge.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Create a class
+ */
+class AchievementBadge
+{
+    /**
+     * Class properties
+     */
+    public $title;
+    public $description;
+    public $point;
+
+    /**
+     * Class method
+     */
+    public function awardTo($user)
+    {
+    }
+}
